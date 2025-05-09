04030ad20b02e874e7e03bbdc9da37d3b85b7966
Quickly Egypt <abdelrahman18251@gmail.com>
2025-05-09 21:17:55 +0300

    full app

--- lib/screens/send_money_steps/index.dart
+++
@@ -1,4 +0,0 @@
-export 'step_one_screen.dart';
-export 'step_two_screen.dart';
-export 'step_three_screen.dart';
-export 'step_four_screen.dart';
--- lib/utils/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class Colors{
-
-  static const Color primaryColor = Color(0xFF031E4B);
-
-
-
-}
